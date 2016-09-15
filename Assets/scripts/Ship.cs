ed9c5b832c72140d2f5fcbb68a6b8c39d619e19d
iDingDong <idingdong@mogara.org>
2016-09-15 16:55:31 +0800

    Add scprits for GameManager and Ship

---
+++ Assets/scripts/Ship.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ship : MonoBehaviour
+{
+	private Transform reservedTransform;
+	
+
+	// Use this for initialization
+	void Start()
+	{
+		reservedTransform = GetComponent<Transform>();
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		
+	}
+
+	void Move(float x, float z)
+	{
+		reservedTransform.Translate(x, 0, z);
+    }
+
+}
