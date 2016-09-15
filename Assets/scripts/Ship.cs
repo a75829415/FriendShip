fa4e8a16669338322f63351d2dddcd795e0cf860
iDingDong <idingdong@mogara.org>
2016-09-15 19:32:58 +0800

    add physics component

--- Assets/scripts/Ship.cs
+++ Assets/scripts/Ship.cs
@@ -4,19 +4,30 @@ using System.Collections;
 public class Ship : MonoBehaviour
 {
 	private Transform reservedTransform;
-	
+	private Rigidbody reservedRigidbody;
+	private ConstantForce reservedBackgroundForce;
+
+	public float accelerationBase = 0.5f;
+
 
 	// Use this for initialization
 	void Start()
 	{
 		reservedTransform = GetComponent<Transform>();
-	}
+		reservedRigidbody = GetComponent<Rigidbody>();
+		reservedBackgroundForce = GetComponent<ConstantForce>();
+    }
 
 	// Update is called once per frame
 	void Update()
 	{
 		
 	}
+
+	private float ToActualForce(float acceleration)
+	{
+		return acceleration * reservedRigidbody.mass;
+    }
 
 	void Move(float x, float z)
 	{
