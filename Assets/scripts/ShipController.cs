b285a091260d9525a25594408760fd69d1ba7405
a75829415 <a75829415@outlook.com>
2016-09-16 12:55:18 +0800

    test of shared control of ship in LAN

---
+++ Assets/scripts/ShipController.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+
+public class ShipController : NetworkBehaviour
+{
+    private Transform ship;
+
+    // Use this for initialization
+    void Start()
+    {
+        ship = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            CmdRotate((isServer ? 5 : -5) * Time.deltaTime);
+        }
+    }
+
+    [ClientRpc]
+    public void RpcRotate(float angle)
+    {
+        ship.Rotate(new Vector3(0, -1, 0), angle);
+    }
+
+    [Command]
+    public void CmdRotate(float angle)
+    {
+        RpcRotate(angle);
+    }
+}
