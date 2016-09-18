16d53e949845adca9403aca9a2592125ad3cd298
iDingDong <idingdong@mogara.org>
2016-09-18 22:25:12 +0800

    Enhance camera rotation

---
+++ Assets/scripts/ChasingCamera.cs
@@ -0,0 +1,109 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChasingCamera : MoveableObject {
+	public Ship ship;
+	public Camera reservedCamera;
+
+	public float maxRadiansDeltaPerSecond;
+	public float maxMagnitudeDeltaPerSecond;
+
+	private Vector3 previousPosition = new Vector3(0, 16, 13);
+	private Vector3 previousRotation = new Vector3(90, 0, 0);
+	private float previousAngle = 0.0f;
+
+	private float GetMaxRadiansDelta()
+	{
+		return maxRadiansDeltaPerSecond * Time.deltaTime;
+    }
+
+	private float GetMaxMagnitudeDelta()
+	{
+		return maxMagnitudeDeltaPerSecond * Time.deltaTime;
+	}
+
+	private float ChasingCameraProgress(float y)
+	{
+		float starting = 50.0f;
+		float result = ship.GetSpeed() + starting;
+		float ratio = 2.0f * Manager.instance.WaitTime / Manager.instance.waitTimeBase - 1.0f;
+		if (ratio > 0)
+		{
+			result -= starting * ratio;
+		}
+		result /= 50.0f;
+		result += 1.0f;
+		return Mathf.Pow(result, y);
+	}
+
+	// Use this for initialization
+	void Start () {
+		base.StartWorkaround();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		UpdateCameraPosition();
+		UpdateCameraRotation();
+		UpdateCameraFOV();
+		UpdateCameraVerticalRotation();
+	}
+
+	private void UpdateCameraPosition()
+	{
+		float x = 0;
+		float y = 3 + 13 / ChasingCameraProgress(2.2f);
+		float z = -3f + 16f / ChasingCameraProgress(2.5f);
+		Vector3 expected = new Vector3(x, y, z);
+        reservedTransform.localPosition = Vector3.RotateTowards(
+			previousPosition, expected, GetMaxRadiansDelta(), GetMaxMagnitudeDelta());
+		previousPosition = reservedTransform.localPosition;
+    }
+
+	private void UpdateCameraRotation()
+	{
+		float x = 15 + 75 / ChasingCameraProgress(1.0f);
+		float y = 0;
+		float z = 0;
+		Vector3 expected = new Vector3(x, y, z);
+		reservedTransform.localEulerAngles = Vector3.RotateTowards(
+			previousRotation, expected, GetMaxRadiansDelta(), GetMaxMagnitudeDelta());
+		previousRotation = reservedTransform.localEulerAngles;
+    }
+
+	private void UpdateCameraFOV()
+	{
+		Vector3 expected = new Vector3(90 + 30 / ChasingCameraProgress(1.0f), 0, 0);
+		reservedCamera.fieldOfView = Vector3.RotateTowards(
+			new Vector3(reservedCamera.fieldOfView, 0, 0), expected, GetMaxRadiansDelta(), GetMaxMagnitudeDelta()).x;
+	}
+
+	private void UpdateCameraVerticalRotation()
+	{
+		float angle = ship.reservedRigidbody.angularVelocity.y;
+		bool toward_left;
+		if (angle < 0)
+		{
+			toward_left = true;
+		}
+		else
+		{
+			toward_left = false;
+		}
+		angle = Mathf.Abs(angle) + 1.0f;
+		angle = 15 - 15 / angle;
+		if (toward_left)
+		{
+			angle = -angle;
+		}
+		Vector3 fakeExpected = new Vector3(angle + 15, 0, 0);
+		float actualAngle = Vector3.RotateTowards(
+			new Vector3(previousAngle + 15, 0, 0), fakeExpected, GetMaxRadiansDelta(), GetMaxMagnitudeDelta()).x - 15;
+        reservedTransform.RotateAround(
+			new Vector3(ship.Position.x, Position.y, ship.Position.z),
+			Vector3.up,
+			actualAngle);
+		previousAngle = actualAngle;
+	}
+
+}
--- Assets/scripts/Ship.cs
+++ Assets/scripts/Ship.cs
@@ -4,11 +4,9 @@ using System.Collections;
 public class Ship : MoveableObject
 {
 	public Collider mainCollider;
-	public Camera thirdPersonCamera;
-    public Transform thirdPersonCameraTransform;
 
-	private Rigidbody reservedRigidbody;
-	private ConstantForce reservedBackgroundForce;
+	public Rigidbody reservedRigidbody;
+	public ConstantForce reservedBackgroundForce;
 
 	public float accelerationBase;
 	public float sideAccelerationBase;
@@ -19,24 +17,14 @@ public class Ship : MoveableObject
 
 	private bool unprepared;
 
-
-	private float ChasingCameraProgress(float y)
-	{
-		float starting = 50.0f;
-		float result = GetSpeed() + starting;
-		float ratio = 2.0f * Manager.instance.WaitTime / Manager.instance.waitTimeBase - 1.0f;
-		if (ratio > 0) {
-			result -= starting * ratio;
-		}
-		result /= 50.0f;
-		result += 1.0f;
-		Debug.Log(Mathf.Pow(result, y));
-        return Mathf.Pow(result, y);
-	}
-
 	public float GetSpeed()
 	{
 		return reservedRigidbody.velocity.magnitude;
+	}
+
+	public float GetVertical()
+	{
+		return reservedRigidbody.angularVelocity.y;
 	}
 
 	public bool isInvincible()
@@ -62,8 +50,6 @@ public class Ship : MoveableObject
 	{
 		unprepared = true;
         base.StartWorkaround();
-		reservedRigidbody = GetComponent<Rigidbody>();
-		reservedBackgroundForce = GetComponent<ConstantForce>();
 		ResetInvincibleStatus();
     }
 
@@ -80,35 +66,6 @@ public class Ship : MoveableObject
 			UpdateInvincibilityStatus();
 			UpdateForce();
 		}
-		UpdateCamera();
-	}
-
-	private void UpdateCamera()
-	{
-		UpdateCameraPosition();
-		UpdateCameraRotation();
-		UpdateCameraFOV();
-    }
-
-	private void UpdateCameraPosition()
-	{
-		float x = 0;
-		float y = 3 + 13 / ChasingCameraProgress(2.2f);
-		float z = -3f + 16f / ChasingCameraProgress(2.5f);
-		thirdPersonCameraTransform.localPosition = new Vector3(x, y, z);
-    }
-
-	private void UpdateCameraRotation()
-	{
-		float x = 15 + 75 /  ChasingCameraProgress(1.0f);
-		float y = 0;
-		float z = 0;
-		thirdPersonCameraTransform.localRotation = Quaternion.Euler(x, y, z);
-    }
-
-	private void UpdateCameraFOV()
-	{
-		thirdPersonCamera.fieldOfView = 90 + 30 / ChasingCameraProgress(1.0f);
 	}
 
 	private void UpdateEngagingStatus()
