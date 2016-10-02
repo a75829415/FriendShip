ec87a9221d0d72a7159239df799033458a9d70c1
a75829415 <a75829415@outlook.com>
2016-10-02 15:51:40 +0800

    do preparations in welcome scene - load DontDestroyOnLoad objects and then change to lobby scene

---
+++ Assets/scripts/lobby/Preparation.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Preparation : MonoBehaviour
+{
+    public GameObject[] crossSceneObjectPrefabs;
+
+    void Awake()
+    {
+        foreach(GameObject obj in crossSceneObjectPrefabs)
+        {
+            DontDestroyOnLoad(Instantiate(obj));
+        }
+    }
+
+    void Start()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
