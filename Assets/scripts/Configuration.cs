cb519aede7ec511581a9e289439e8b4b50cd8d3d
iDingDong <idingdong@mogara.org>
2016-10-07 14:07:48 +0800

    Add Configuration

---
+++ Assets/scripts/Configuration.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+
+public static class Configuration {
+	public static uint health = 3;
+
+	public static uint numberOfPlayers = 1;
+
+	public static GameMode mode = GameMode.Classic;
+	
+}
